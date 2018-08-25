59ddecfa4d2ffbce0e80d79b9d07d06937af6eaa
pzpsc <pzubkiewicz@ptc.com>
2018-08-25 13:02:17 +0200

    not needed

--- src/handler.js
+++
@@ -1,4 +0,0 @@
-module.exports.main = (event, context, callback) => {
-  const response = { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-  callback(null, response)
-}
