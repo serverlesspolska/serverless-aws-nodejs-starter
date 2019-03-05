9d4b6d2d4c6a1c3fb10dbb11223781b1a083f37c
Pawel Z <pawel@zubkiewicz.com>
2019-03-05 11:21:44 +0100

    disabled eslint warnings

--- src/sampleOne/lambda.js
+++ src/sampleOne/lambda.js
@@ -1,5 +1,6 @@
 const helper = require('./helper')
 
+// eslint-disable-next-line no-unused-vars
 module.exports.main = async (event, context) => {
   const response = { message: helper('World') };
   return response
--- src/sampleTwo/lambda.js
+++ src/sampleTwo/lambda.js
@@ -1,5 +1,6 @@
 const responses = require('./responses')
 
+// eslint-disable-next-line no-unused-vars
 module.exports.main = async (event, context) => {
   // context.callbackWaitsForEmptyEventLoop = false
   const { MSG } = process.env
